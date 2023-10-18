fe371bc3f0abdff2f4c04a5e881d727525ab6c45
Mela Maluenda <maluenda.mela@gmail.com>
2023-10-19 00:20:11 +0200

    Create character instances

--- src/Components/App/App.ts
+++ src/Components/App/App.ts
@@ -1,3 +1,5 @@
+import CharacterCard from "../CharacterCard/CharacterCard.js";
+import { characters } from "../characters/characters.js";
 import Component from "../component/component.js";
 
 class App extends Component {
@@ -6,6 +8,46 @@ class App extends Component {
     </ul>
     
     `;
+    const parentCard = document.querySelector(".characters-list")!;
+    const cardJoffrey = new CharacterCard(
+      parentCard,
+      "li",
+      "col",
+      characters[0].characterData,
+    );
+    cardJoffrey.render();
+
+    const cardJaime = new CharacterCard(
+      parentCard,
+      "li",
+      "col",
+      characters[1].characterData,
+    );
+    cardJaime.render();
+
+    const cardDaenerys = new CharacterCard(
+      parentCard,
+      "li",
+      "col",
+      characters[2].characterData,
+    );
+    cardDaenerys.render();
+
+    const cardTyron = new CharacterCard(
+      parentCard,
+      "li",
+      "col",
+      characters[3].characterData,
+    );
+    cardTyron.render();
+
+    const cardBronn = new CharacterCard(
+      parentCard,
+      "li",
+      "col",
+      characters[4].characterData,
+    );
+    cardBronn.render();
   }
 }
 
