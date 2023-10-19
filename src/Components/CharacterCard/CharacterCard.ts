ab7555a8a5910e9f62b9bd43edb39c5046ed85b1
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-19 22:38:21 +0200

    Add upside down images for dead characters

--- src/Components/CharacterCard/CharacterCard.ts
+++ src/Components/CharacterCard/CharacterCard.ts
@@ -1,8 +1,10 @@
 import Component from "../component/component.js";
 import type CharacterData from "../../types.js";
+import Character from "../Character/Character.js";
 
 class CharacterCard extends Component {
   characterData;
+  state: boolean;
 
   constructor(
     parentElement: Element,
@@ -12,15 +14,22 @@ class CharacterCard extends Component {
   ) {
     super(parentElement, tagName, className);
     this.characterData = data;
+    this.state = this.state;
   }
 
   protected populate(): void {
     this.element.innerHTML = ` 
       <article class="character">
           <div class="card character__card">
-            <img src="img/${this.characterData.imageSource}" alt="Character's Name and family" class="character__picture card-img-top" />
+            <img src="img/${
+              this.characterData.imageSource
+            }" alt="Character's Name and family" class="character__picture card-img-top" ${
+              this.state ? "" : "character__upside-down-picture"
+            } } />
             <div class="card-body">
-              <h2 class="character__name card-title h4">${this.characterData.name}${this.characterData.family}</h2>
+              <h2 class="character__name card-title h4">${
+                this.characterData.name
+              }${this.characterData.family}</h2>
               <div class="character__info">
                 <ul class="list-unstyled">
                   <li>Age: ${this.characterData.age} yrs</li>
