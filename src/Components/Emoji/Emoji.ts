f03a397d85ab8b4afd7294a46f625273b9901cff
Mela Maluenda <maluenda.mela@gmail.com>
2023-10-19 23:05:50 +0200

    Create a new emoji character's class

---
+++ src/Components/Emoji/Emoji.ts
@@ -0,0 +1,10 @@
+import CharacterData from "../../types.js";
+
+class Emoji extends CharacterData {
+  emojiKing: "👑";
+  emojiFighter: "🗡";
+  emojiAdviser: "🎓";
+  emojiSquire: "🛡";
+}
+
+export default Emoji;
